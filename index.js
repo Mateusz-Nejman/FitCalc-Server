48f445b7d2d97e0907dbd548fab1bc0336312576
Mateusz Nejman <mateusz_nejman@hotmail.com>
2020-03-15 20:39:11 +0100

    quick fix

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
 let not_verifiedHashes = [];
 let to_verifyHashes = [];
